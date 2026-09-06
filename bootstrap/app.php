14e71123275ac9a1bc7021816168fb74814dab4e
HanandityaKen <hanandityakenanta@gmail.com>
2026-09-06 12:19:03 +0700

    Call useStoragePath on Application instance after create in Laravel 11

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,8 +13,7 @@ if (!is_dir($storagePath)) {
     @mkdir($storagePath . '/logs', 0777, true);
 }
 
-return Application::configure(basePath: dirname(__DIR__))
-    ->useStoragePath($storagePath)
+$app = Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -27,3 +26,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
+
+$app->useStoragePath($storagePath);
+
+return $app;
