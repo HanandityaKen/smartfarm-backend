39de86a144c6cdb218401b03248d9660c11b5223
HanandityaKen <hanandityakenanta@gmail.com>
2026-09-06 12:09:19 +0700

    Hardcode timezone Asia/Jakarta and disable PHP notices display

--- api/index.php
+++ api/index.php
@@ -1,13 +1,13 @@
 <?php
 
-// Force valid APP_TIMEZONE environment variable for Laravel bootstrap on Vercel
-if (empty($_ENV['APP_TIMEZONE']) || $_ENV['APP_TIMEZONE'] === '""' || $_ENV['APP_TIMEZONE'] === "''") {
-    $_ENV['APP_TIMEZONE'] = 'Asia/Jakarta';
-    $_SERVER['APP_TIMEZONE'] = 'Asia/Jakarta';
-    putenv('APP_TIMEZONE=Asia/Jakarta');
-}
+// Turn off display of PHP notices/warnings in production output
+ini_set('display_errors', '0');
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING & ~E_DEPRECATED);
 
 date_default_timezone_set('Asia/Jakarta');
+putenv('APP_TIMEZONE=Asia/Jakarta');
+$_ENV['APP_TIMEZONE'] = 'Asia/Jakarta';
+$_SERVER['APP_TIMEZONE'] = 'Asia/Jakarta';
 
 // Forward request to Laravel public/index.php for Vercel Serverless Function
 require __DIR__ . '/../public/index.php';
