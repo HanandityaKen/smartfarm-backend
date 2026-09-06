53f39105f1a8acf7574b72a54e822235e1fb7f1b
HanandityaKen <hanandityakenanta@gmail.com>
2026-09-06 12:06:12 +0700

    Fix timezone fallback and set JSON root response

--- config/app.php
+++ config/app.php
@@ -65,20 +65,9 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'UTC'),
+    'timezone' => env('APP_TIMEZONE') ?: 'Asia/Jakarta',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Application Locale Configuration
-    |--------------------------------------------------------------------------
-    |
-    | The application locale determines the default locale that will be used
-    | by Laravel's translation / localization methods. This option can be
-    | set to any locale for which you plan to have translation strings.
-    |
-    */
-
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE') ?: 'en',
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
