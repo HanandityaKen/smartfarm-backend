2b6e4eea405be8b5d2755a8de2030fbc8103f091
HanandityaKen <hanandityakenanta@gmail.com>
2026-09-06 12:07:36 +0700

    Override APP_TIMEZONE in api/index.php for Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,13 @@
 <?php
 
+// Force valid APP_TIMEZONE environment variable for Laravel bootstrap on Vercel
+if (empty($_ENV['APP_TIMEZONE']) || $_ENV['APP_TIMEZONE'] === '""' || $_ENV['APP_TIMEZONE'] === "''") {
+    $_ENV['APP_TIMEZONE'] = 'Asia/Jakarta';
+    $_SERVER['APP_TIMEZONE'] = 'Asia/Jakarta';
+    putenv('APP_TIMEZONE=Asia/Jakarta');
+}
+
+date_default_timezone_set('Asia/Jakarta');
+
 // Forward request to Laravel public/index.php for Vercel Serverless Function
 require __DIR__ . '/../public/index.php';
